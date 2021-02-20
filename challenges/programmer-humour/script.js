5391b271aff94f53c94258861cf347f838ba2a32
Chizim <garrohat@yahoo.co.uk>
2021-02-20 22:04:29 +0000

    Completed Challenges

---
+++ challenges/programmer-humour/script.js
@@ -0,0 +1,13 @@
+let divEl; 
+function getData(){
+    divEl = document.getElementById("div");
+    fetch("https://xkcd.now.sh/?comic=latest")
+    .then(response => response.json())
+    .then(data => {
+        data
+        divEl.innerHTML = `<h3>${data.title}</h3><img src = ${data.img} alt"Random dog photos">`;
+    })
+    .catch(error => console.log(error));
+}
+
+window.onload = getData;
